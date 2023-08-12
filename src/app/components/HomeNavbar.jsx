3691c4b0fb528d84c94718aae261788d31a853b6
am094137 <aniket.mehta@cerner.com>
2023-08-12 15:36:13 +0530

    initial setup

---
+++ src/app/components/HomeNavbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HomeNavbar = () => {
+  return (
+    <div className='bg-black py-4'>
+        <div className="container mx-auto px-4">
+            <div className='flex justify-between items-center'>
+                <div className='flex items-center'>
+                    /* Logo */
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default HomeNavbar
